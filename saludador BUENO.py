da6543862f1d0e3685cc5a217b2fc0778971078d
MrEspina <90762396+MrEspina@users.noreply.github.com>
2021-10-18 10:33:11 +0200

    Delete saludador BUENO.py

--- saludador BUENO.py
+++
@@ -1,9 +0,0 @@
-def saludador ():
-    a=raw_input("Introduce tu nombre: ")
-    b=raw_input("Introduce el nombre de tu lugar de residencia: ")
-    c=input("Introduce tu edad: ")
-    print("Te llamas "+a+", vives en "+b+","+" tu edad es de "+str(c)+" anios")
-    print("Ten cuidado.")
-    print("Adios.")
-    
-saludador()
