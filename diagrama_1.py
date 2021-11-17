ccf1f097283cb3aa11923b53eb4496d71b7de641
MrEspina <90762396+MrEspina@users.noreply.github.com>
2021-11-17 11:54:52 +0100

    Add files via upload

---
+++ diagrama_1.py
@@ -0,0 +1,10 @@
+def diagrama_1():
+    cont=0
+    y=0
+    x=input("Dame un numero: ")
+    while(cont<11):
+        y=cont*x
+        print(str(cont)+"x"+str(x)+" = "+str(y))
+        cont=cont+1
+    print("That´s all folKs.")
+diagrama_1()
---
+++ diagrama_2.py
@@ -0,0 +1,9 @@
+def diagrama_2():
+    num=input("Dame un numero: ")
+    sumita=1
+    for cont in range(num,-1,-1):
+        print(cont,sumita)
+        sumita=sumita*cont
+diagrama_2()
+        
+    
